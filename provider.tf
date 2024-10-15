1eb27af2cbbaba89c6d8ed289bc21f2e7b932cff
Administrator <admin@example.com>
2024-10-15 15:26:43 +0300

    add public key-2

--- provider.tf
+++ provider.tf
@@ -9,6 +9,10 @@
 
 #   required_version = ">= 0.12"
 # }
+variable "google_creds" {
+  description = "GCP credentials JSON"
+  type        = string
+}
 
 provider "google" {
   project = "my-project-377213"
